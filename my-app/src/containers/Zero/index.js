631d098d39691ee37cdd71374942b2417eb6342c
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-07-01 14:01:59 +0100

    a

--- my-app/src/containers/Zero/index.js
+++ my-app/src/containers/Zero/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Solids from "./Solids";
-import { Box } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
 
 const Zero = () => {
   return (
     <Box style={{ height: "100vh", backgroundColor: "#17202A" }}>
+      <Box
+        bgcolor='#00000066'
+        style={{
+          position: "absolute",
+          padding: "10px",
+          borderRadius: "5px",
+          top: "15%",
+          left: "5%",
+          zIndex: 3,
+        }}
+      >
+        <Typography style={{ color: "#ffffffCE", fontSize: "45px" }}>
+          Pedro Sena Rego
+        </Typography>
+        <Typography style={{ color: "#ffffffCE", fontSize: "20px" }}>
+          Front End Developer & 3D Designer
+        </Typography>
+      </Box>
       <Solids />
     </Box>
   );
